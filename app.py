ed319493c831e127a9b92298f0ec80377e26e5cf
Beau Colley-Allerton <beau.colley-allerton@boeing.com>
2024-12-04 17:18:23 +1100

    Added flag to switch projects

--- app.py
+++ app.py
@@ -15,29 +15,34 @@ import re
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 
-GITLAB_URL = config['gitlab']['url']
-PROJECT_ID = config['gitlab']['project_id']
-GROUP_ID = config['gitlab']['group_id']
-PRIVATE_TOKEN = config['gitlab']['access_token']
-
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler("gitlab_assistant.log"), logging.StreamHandler()])
 
 logger = logging.getLogger(__name__)
 
-# Connect to GitLab
-gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
+
+def load_project_config(project_name):
+    if project_name not in config:
+        raise ValueError(f"Project {project_name} not found in configuration.")
+    project_config = config[project_name]
+    return project_config['url'], project_config['project_id'], project_config['group_id'], project_config['access_token']
+
 
 @click.group()
 def cli():
     """Command line tool for managing GitLab issues."""
     pass
 
+
 @cli.command()
+@click.option('--project_name', required=True, help='Name of the project to use.')
 @click.option('--output', default='open_issues.csv', help='Output CSV file for open issues.')
-def pull_issues(output):
+def pull_issues(project_name, output):
     """Fetch and export all open issues to a CSV file."""
+    GITLAB_URL, PROJECT_ID, GROUP_ID, PRIVATE_TOKEN = load_project_config(project_name)
+    gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
+
     # Get the project
     project = gl.projects.get(PROJECT_ID)
 
@@ -51,7 +56,6 @@ def pull_issues(output):
         writer.writeheader()
         for issue in open_issues:
             logger.info(f"Processing issue {issue.id} - {issue.title}")
-            # pdb.set_trace()
             writer.writerow({
                 'id': issue.id,
                 'iid': issue.iid,
@@ -69,128 +73,14 @@ def pull_issues(output):
 
     click.echo(f'Open issues exported to {output}')
 
-
-def get_iteration_id(start_date):
-    iterations = gl.groups.get(GROUP_ID).iterations.list(get_all=True)
-    for iteration in iterations:
-        # pdb.set_trace()
-        logger.info(iteration.attributes['start_date'])
-        if start_date.strip() == iteration.attributes['start_date']:
-            return iteration.iid
-    logger.error(f"Iteration with start date {start_date} not found")
-    pass
-
-
-def get_epic(epic_title):
-    epics = gl.groups.get(GROUP_ID).epics.list(get_all=True, state='opened')
-    logger.debug(f'epics: {epics}')
-    for epic in epics:
-        if epic.title.strip().lower() == epic_title.strip().lower():
-            return gl.groups.get(GROUP_ID).epics.get(epic.iid)
-    logger.error(f'epic {epic_title} not found.')
-    return None
-
-
-def get_milestone_id(milestone_title):
-    milestones = gl.groups.get(GROUP_ID).milestones.list(state='active', get_all=True)
-    logger.info(f'milestones: {milestones}')
-
-    for milestone in milestones:
-        if milestone.title.strip().lower() == milestone_title.strip().lower():
-            return milestone.get_id()
-    logger.error(f'milestone {milestone_title} not found.')
-    return None
-
-
-def update_issue(issue, row):
-    logger.info(f"Updating issue ID {issue.iid} - {issue.title}")
-    issue_updated = False
-
-    for key, value in row.items():
-        if key in issue.attributes and str(getattr(issue, key, '')).strip() != str(value).strip() and value != "":
-            if key == 'author':
-                continue
-
-            if key == 'iteration':
-                continue
-
-            if key == 'labels':
-                continue
-
-            if key == 'epic':
-                # Remove epic from issue if csv value is empty or different
-                if value == "" or (issue.epic is not None and value != issue.epic["title"]):
-                    logger.info(f"Removing epic {issue.epic['title']}")
-                    issue.epic = None
-                    issue_updated = True
-                    continue
-
-                # Check if epic is already set
-                if issue.epic is not None and value == issue.epic["title"]:
-                    continue
-
-                # Set epic
-                epic = get_epic(value)
-                if epic is not None:
-                    epic.issues.create({'issue_id': issue.id})
-                    logger.info(f"Added issue to epic {value}")
-                continue
-
-            if key == 'milestone':
-                # Remove milestone from issue if csv value is empty or different
-                if value == "" or (issue.milestone is not None and value != issue.milestone["title"]):
-                    logger.info(f"Removing milestone {issue.milestone['title']}")
-                    issue.milestone = None
-                    issue_updated = True
-                    continue
-
-                # Check if milestone is already set
-                if issue.milestone is not None and value == issue.milestone["title"]:
-                    continue
-
-                # Set milestone
-                milestone_id = get_milestone_id(value)
-                issue.milestone_id = milestone_id
-                issue_updated = True
-                continue
-
-            logger.info(f"Updating {key} from {getattr(issue, key, '')} to {value}")
-            setattr(issue, key, value)
-            issue_updated = True
-
-    if issue_updated:
-        # pdb.set_trace()
-        issue.save()
-
-
-def create_issue(row):
-    logger.info(f"Creating new issue {row['title']}")
-    new_issue_data = {
-        'title': row['title'],
-        'description': row['description'],
-        'state': row['state'],
-        'weight': row['weight']
-    }
-
-    if row['epic']:
-        epic = get_epic(row['epic'])
-        if epic is not None:
-            new_issue_data['epic_id'] = epic.id
-
-    if row['milestone']:
-        milestone_id = get_milestone_id(row['milestone'])
-        if milestone_id is not None:
-            new_issue_data['milestone_id'] = milestone_id
-
-    project = gl.projects.get(PROJECT_ID)
-    logger.info(f"Creating new issue {new_issue_data}") 
-    project.issues.create(new_issue_data)
-
-
 @cli.command()
+@click.option('--project_name', required=True, help='Name of the project to use.')
 @click.option('--input', required=True, help='Input CSV file to update issues.')
-def update_issues(input):
+def update_issues(project_name, input):
     """Update issues from a CSV file."""
+    GITLAB_URL, PROJECT_ID, GROUP_ID, PRIVATE_TOKEN = load_project_config(project_name)
+    gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
+
     # Read the CSV file
     with open(input, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -204,20 +94,24 @@ def update_issues(input):
         issue_id = row['iid']  # Convert ID to integer
 
         if issue_id == "":
-            create_issue(row)
+            create_issue(gl, project, row)
         else:
             try:
                 issue = project.issues.get(int(issue_id))
-                update_issue(issue, row)
+                update_issue(gl, project, issue, row)
             except gitlab.exceptions.GitlabGetError as e:
                 click.echo(f'Issue ID {issue_id} not found - {e}')
             except gitlab.exceptions.GitlabUpdateError as e:
                 click.echo(f'Issue ID {issue_id} could not be updated - {e}')
 
 @cli.command()
+@click.option('--project_name', required=True, help='Name of the project to use.')
 @click.option('--input', required=True, help='Input CSV file to determine issues to keep.')
-def close_issues(input):
+def close_issues(project_name, input):
     """Close issues that are not present in the input CSV file."""
+    GITLAB_URL, PROJECT_ID, GROUP_ID, PRIVATE_TOKEN = load_project_config(project_name)
+    gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
+
     # Read the CSV file
     with open(input, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -251,16 +145,12 @@ def close_issues(input):
     else:
         click.echo('No issues to close.')
 
-
-def format_time(hours: float) -> str:
-    hours_int = int(hours)
-    minutes = int((hours - hours_int) * 60)
-    return f'{hours_int}h{minutes}m'
-
-
 @cli.command()
-def log_time():
+@click.option('--project_name', required=True, help='Name of the project to use.')
+def log_time(project_name):
     """Log time spent on an issue."""
+    GITLAB_URL, PROJECT_ID, GROUP_ID, PRIVATE_TOKEN = load_project_config(project_name)
+    gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
 
     # Get the project
     project = gl.projects.get(PROJECT_ID)
@@ -303,7 +193,6 @@ def log_time():
     issues_by_user = defaultdict(list)
 
     for issue in filtered_issues:
-        # print(issue.title)
         assignees = issue.assignees
         for assignee in assignees:
             issues_by_user[assignee['username']].append(issue)
@@ -315,6 +204,126 @@ def log_time():
         for issue in issues:
             logger.info(f"{issue.iid} - {issue.title} - {issue.labels}")
             issue.add_spent_time(time_per_ticket)
+
+
+def format_time(hours: float) -> str:
+    hours_int = int(hours)
+    minutes = int((hours - hours_int) * 60)
+    return f'{hours_int}h{minutes}m'
+
+
+def get_iteration_id(gl, group_id, start_date):
+    iterations = gl.groups.get(group_id).iterations.list(get_all=True)
+    for iteration in iterations:
+        logger.info(iteration.attributes['start_date'])
+        if start_date.strip() == iteration.attributes['start_date']:
+            return iteration.iid
+    logger.error(f"Iteration with start date {start_date} not found")
+    pass
+
+
+def get_epic(gl, group_id, epic_title):
+    epics = gl.groups.get(group_id).epics.list(get_all=True, state='opened')
+    logger.debug(f'epics: {epics}')
+    for epic in epics:
+        if epic.title.strip().lower() == epic_title.strip().lower():
+            return gl.groups.get(group_id).epics.get(epic.iid)
+    logger.error(f'epic {epic_title} not found.')
+    return None
+
+
+def get_milestone_id(gl, group_id, milestone_title):
+    milestones = gl.groups.get(group_id).milestones.list(state='active', get_all=True)
+    logger.info(f'milestones: {milestones}')
+
+    for milestone in milestones:
+        if milestone.title.strip().lower() == milestone_title.strip().lower():
+            return milestone.get_id()
+    logger.error(f'milestone {milestone_title} not found.')
+    return None
+
+
+def update_issue(gl, project, issue, row):
+    logger.info(f"Updating issue ID {issue.iid} - {issue.title}")
+    issue_updated = False
+
+    for key, value in row.items():
+        if key in issue.attributes and str(getattr(issue, key, '')).strip() != str(value).strip() and value != "":
+            if key == 'author':
+                continue
+
+            if key == 'iteration':
+                continue
+
+            if key == 'labels':
+                continue
+
+            if key == 'epic':
+                # Remove epic from issue if csv value is empty or different
+                if value == "" or (issue.epic is not None and value != issue.epic["title"]):
+                    logger.info(f"Removing epic {issue.epic['title']}")
+                    issue.epic = None
+                    issue_updated = True
+                    continue
+
+                # Check if epic is already set
+                if issue.epic is not None and value == issue.epic["title"]:
+                    continue
+
+                # Set epic
+                epic = get_epic(gl, project.namespace['id'], value)
+                if epic is not None:
+                    epic.issues.create({'issue_id': issue.id})
+                    logger.info(f"Added issue to epic {value}")
+                continue
+
+            if key == 'milestone':
+                # Remove milestone from issue if csv value is empty or different
+                if value == "" or (issue.milestone is not None and value != issue.milestone["title"]):
+                    logger.info(f"Removing milestone {issue.milestone['title']}")
+                    issue.milestone = None
+                    issue_updated = True
+                    continue
+
+                # Check if milestone is already set
+                if issue.milestone is not None and value == issue.milestone["title"]:
+                    continue
+
+                # Set milestone
+                milestone_id = get_milestone_id(gl, project.namespace['id'], value)
+                issue.milestone_id = milestone_id
+                issue_updated = True
+                continue
+
+            logger.info(f"Updating {key} from {getattr(issue, key, '')} to {value}")
+            setattr(issue, key, value)
+            issue_updated = True
+
+    if issue_updated:
+        issue.save()
+
+
+def create_issue(gl, project, row):
+    logger.info(f"Creating new issue {row['title']}")
+    new_issue_data = {
+        'title': row['title'],
+        'description': row['description'],
+        'state': row['state'],
+        'weight': row['weight']
+    }
+
+    if row['epic']:
+        epic = get_epic(gl, project.namespace['id'], row['epic'])
+        if epic is not None:
+            new_issue_data['epic_id'] = epic.id
+
+    if row['milestone']:
+        milestone_id = get_milestone_id(gl, project.namespace['id'], row['milestone'])
+        if milestone_id is not None:
+            new_issue_data['milestone_id'] = milestone_id
+
+    logger.info(f"Creating new issue {new_issue_data}") 
+    project.issues.create(new_issue_data)
 
 
 if __name__ == '__main__':
